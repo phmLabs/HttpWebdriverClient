ee4ab2ebc79d62b5d295fce0e50cc406495c25bc
phmLabs <nils.langner@phmlabs.com>
2017-04-02 19:13:28 +0200

    psr cache

---
+++ src/Http/ChromeClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nils.langner
+ * Date: 02.04.17
+ * Time: 19:08
+ */
+
+namespace phm\HttpWebdriverClient\Http;
+
+
+class ChromeClient
+{
+
+}
---
+++ src/Http/Decorator/CacheDecorator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: nils.langner
+ * Date: 02.04.17
+ * Time: 19:08
+ */
+class CacheDecorator
+{
+
+}
