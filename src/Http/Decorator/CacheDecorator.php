7d9ea250b15e4cd82eb40f88628edd081495d2a7
phmLabs <nils.langner@phmlabs.com>
2017-04-02 20:05:53 +0200

    expires at as param

--- src/Http/Decorator/CacheDecorator.php
+++ src/Http/Decorator/CacheDecorator.php
@@ -13,6 +13,8 @@ class CacheDecorator implements HttpClient
     private $cacheItemPool;
     private $client;
 
+    private $expiresAfter;
+
     private function getHash(RequestInterface $request)
     {
         return md5((string)$request->getUri() . json_encode($request->getHeaders()) . $request->getMethod());
@@ -28,8 +30,13 @@ class CacheDecorator implements HttpClient
         return unserialize($serialzedResponse);
     }
 
-    public function __construct(HttpClient $client, CacheItemPoolInterface $cacheItemPool)
+    public function __construct(HttpClient $client, CacheItemPoolInterface $cacheItemPool, $expiresAfter = null)
     {
+        if (!$expiresAfter) {
+            $this->expiresAfter = new \DateInterval('PT5M');
+        } else {
+            $this->expiresAfter = $expiresAfter;
+        }
         $this->cacheItemPool = $cacheItemPool;
         $this->client = $client;
     }
@@ -45,7 +52,7 @@ class CacheDecorator implements HttpClient
             $response = $this->client->sendRequest($request);
             $cacheItem = new CacheItem($key);
             $cacheItem->set($this->serialzeResponse($response));
-            $cacheItem->expiresAfter(new \DateInterval('P5M'));
+            $cacheItem->expiresAfter($this->expiresAfter);
             $this->cacheItemPool->save($cacheItem);
             return $response;
         }
