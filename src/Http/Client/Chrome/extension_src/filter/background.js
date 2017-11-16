465b3c46cd81febe6a2213b857974baf9bc9c863
Nils Langner <nils.langner@leankoala.com>
2017-11-16 13:56:17 +0100

    double click

--- src/Http/Client/Chrome/extension_src/filter/background.js
+++ src/Http/Client/Chrome/extension_src/filter/background.js
@@ -1,7 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function () {
         return {redirectUrl: 'https://www.example.com'};
-        return {cancel: true};
     },
     {
         urls: [
@@ -9,6 +8,8 @@ chrome.webRequest.onBeforeRequest.addListener(
             "*://*.google-analytics.com/r/collect*",
             "*://*.google-analytics.com/__utm.gif*",
             "*://*.google-analytics.com/r/__utm.gif*",
+            "*://stats.g.doubleclick.net/collect*",
+            "*://stats.g.doubleclick.net/r/collect*",
             "https://*.webtrekk.net/*/wt*"
         ]
     },
