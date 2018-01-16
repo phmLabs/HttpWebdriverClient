5484a90b8bb28554a028cf8f7cddf03599030ff8
Nils Langner <nils.langner@leankoala.com>
2018-01-16 18:58:06 +0100

    merge

--- src/Http/Response/DomAwareResponse.php
+++ src/Http/Response/DomAwareResponse.php
@@ -6,7 +6,13 @@ use Psr\Http\Message\ResponseInterface;
 
 interface DomAwareResponse extends ResponseInterface
 {
+    /**
+     * @return string
+     */
     public function getHtmlBody();
 
+    /**
+     * @return string
+     */
     public function getDomBody();
 }
